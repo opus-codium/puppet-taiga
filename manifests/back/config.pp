d83e15c39d9012cc608afc791a0cd4fbea52e43e
Romain Tartière <romain@blogreen.org>
2016-04-22 11:21:54 +0200

    Use concat to simplify configuration.

--- manifests/back/config.pp
+++ manifests/back/config.pp
@@ -9,10 +9,15 @@ class taiga::back::config {
 
   $secret_key = $taiga::back::secret_key
 
-  file { "${taiga::back::install_dir}/settings/local.py":
-    ensure  => present,
-    owner   => $taiga::back::user,
-    group   => 'nogroup',
+  concat { "${taiga::back::install_dir}/settings/local.py":
+    ensure => present,
+    owner  => $taiga::back::user,
+    group  => 'nogroup',
+  }
+
+  concat::fragment { 'taiga-back-local.py-base':
+    target  => "${taiga::back::install_dir}/settings/local.py",
     content => template('taiga/back/local.py.erb'),
+    order   => '00',
   }
 }
