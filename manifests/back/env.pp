b5b7e7414e572f18a0c0a95e1a28c491cdd36576
Romain Tartière <romain@blogreen.org>
2016-04-30 20:00:04 +0200

    Adjust path for FreeBSD :heart:.

--- manifests/back/env.pp
+++ manifests/back/env.pp
@@ -1,10 +1,20 @@
 class taiga::back::env {
   include taiga::back
 
+  $virtualenv = $::osfamily ? {
+    'Debian'  => '/usr/bin/virtualenv',
+    'FreeBSD' => '/usr/local/bin/virtualenv',
+  }
+
+  $python3 = $::osfamily ? {
+    'Debian'  => '/usr/bin/python3.4',
+    'FreeBSD' => '/usr/local/bin/python3.4',
+  }
+
   exec { 'taiga-back-virtualenv':
-    command     => "/usr/bin/virtualenv -p /usr/bin/python3.4 ${taiga::back::install_dir}",
-    creates     => "${taiga::back::install_dir}/lib",
-    user        => $taiga::back::user,
+    command => "${virtualenv} -p ${python3} ${taiga::back::install_dir}",
+    creates => "${taiga::back::install_dir}/lib/python3.4/site-packages/django",
+    user    => $taiga::back::user,
   }
 
   exec { 'taiga-back-pip-install':
