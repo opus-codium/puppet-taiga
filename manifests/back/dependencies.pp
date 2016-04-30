1d04191148c0b7db73894ccf90b1485458cd0f9b
Romain Tartière <romain@blogreen.org>
2016-04-30 20:03:50 +0200

    First attempt to sort out dependencies.

--- manifests/back/dependencies.pp
+++ manifests/back/dependencies.pp
@@ -1,5 +1,12 @@
 class taiga::back::dependencies {
-  $depends = ['build-essential', 'binutils-doc', 'autoconf', 'flex', 'bison', 'libjpeg-dev', 'libfreetype6-dev', 'libzmq3-dev', 'libgdbm-dev', 'libncurses5-dev', 'automake', 'libtool', 'libffi-dev', 'curl', 'gettext', 'python3', 'python3-pip', 'python-dev', 'python3-dev', 'python-pip', 'virtualenvwrapper', 'libxml2-dev', 'libxslt1-dev']
+  case $::osfamily {
+    'Debian': {
+      $depends = ['build-essential', 'binutils-doc', 'autoconf', 'flex', 'bison', 'libjpeg-dev', 'libfreetype6-dev', 'libzmq3-dev', 'libgdbm-dev', 'libncurses5-dev', 'automake', 'libtool', 'libffi-dev', 'curl', 'gettext', 'python3', 'python3-pip', 'python-dev', 'python3-dev', 'python-pip', 'virtualenvwrapper', 'libxml2-dev', 'libxslt1-dev']
+    }
+    'FreeBSD': {
+      $depends = ['python3', 'py27-virtualenvwrapper', 'libxml2', 'libxslt']
+    }
+  }
 
   package { $depends:
     ensure => installed,
