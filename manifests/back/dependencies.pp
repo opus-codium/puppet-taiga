9f6339553833d1dd9a2fd2fd18d3c97c01847f79
Romuald Conty <neomilium@gmail.com>
2016-05-25 18:12:32 +0200

    Fix missing Debian dependency (libpq-dev)

--- manifests/back/dependencies.pp
+++ manifests/back/dependencies.pp
@@ -1,7 +1,7 @@
 class taiga::back::dependencies {
   case $::osfamily {
     'Debian': {
-      $depends = ['build-essential', 'binutils-doc', 'autoconf', 'flex', 'bison', 'libjpeg-dev', 'libfreetype6-dev', 'libzmq3-dev', 'libgdbm-dev', 'libncurses5-dev', 'automake', 'libtool', 'libffi-dev', 'curl', 'gettext', 'python3', 'python3-pip', 'python-dev', 'python3-dev', 'python-pip', 'virtualenvwrapper', 'libxml2-dev', 'libxslt1-dev']
+      $depends = ['build-essential', 'binutils-doc', 'autoconf', 'flex', 'bison', 'libjpeg-dev', 'libfreetype6-dev', 'libzmq3-dev', 'libgdbm-dev', 'libncurses5-dev', 'automake', 'libtool', 'libffi-dev', 'curl', 'gettext', 'python3', 'python3-pip', 'python-dev', 'python3-dev', 'python-pip', 'virtualenvwrapper', 'libxml2-dev', 'libxslt1-dev', 'libpq-dev']
     }
     'FreeBSD': {
       $depends = ['python3', 'py27-virtualenvwrapper', 'libxml2', 'libxslt']
