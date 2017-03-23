43cc4799a15208d5b7a705be4a7ee1cdd86bf1d8
Romain Tartière <romain@blogreen.org>
2017-03-23 12:22:07 +0100

    Style: use a single dependency per line

--- manifests/back/dependencies.pp
+++ manifests/back/dependencies.pp
@@ -1,10 +1,40 @@
 class taiga::back::dependencies {
   case $::osfamily {
     'Debian': {
-      $depends = ['build-essential', 'binutils-doc', 'autoconf', 'flex', 'bison', 'libjpeg-dev', 'libfreetype6-dev', 'libzmq3-dev', 'libgdbm-dev', 'libncurses5-dev', 'automake', 'libtool', 'libffi-dev', 'curl', 'gettext', 'python3', 'python3-pip', 'python-dev', 'python3-dev', 'python-pip', 'virtualenvwrapper', 'libxml2-dev', 'libxslt1-dev', 'libpq-dev']
+      $depends = [
+        'build-essential',
+        'binutils-doc',
+        'autoconf',
+        'flex',
+        'bison',
+        'libjpeg-dev',
+        'libfreetype6-dev',
+        'libzmq3-dev',
+        'libgdbm-dev',
+        'libncurses5-dev',
+        'automake',
+        'libtool',
+        'libffi-dev',
+        'curl',
+        'gettext',
+        'python3',
+        'python3-pip',
+        'python-dev',
+        'python3-dev',
+        'python-pip',
+        'virtualenvwrapper',
+        'libxml2-dev',
+        'libxslt1-dev',
+        'libpq-dev'
+      ]
     }
     'FreeBSD': {
-      $depends = ['python3', 'py27-virtualenvwrapper', 'libxml2', 'libxslt']
+      $depends = [
+        'python3',
+        'py27-virtualenvwrapper',
+        'libxml2',
+        'libxslt'
+      ]
     }
     default: {
       fail("Unsupported operating system: ${::osfamily}")
