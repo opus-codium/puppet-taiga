5a7948aa9fa9704499ada51d3cc79ec7aa089634
Romuald Conty <neomilium@gmail.com>
2017-07-19 10:05:04 +0200

    Give access to the global site-packages modules to the virtual environment
    This commit allow to use system-wide already available python packages from taiga-back virtual environment

--- manifests/back/env.pp
+++ manifests/back/env.pp
@@ -2,7 +2,7 @@ class taiga::back::env {
   include ::taiga::back
 
   exec { 'taiga-back-virtualenv':
-    command => "${taiga::back::virtualenv} -p ${taiga::back::python_path}${taiga::back::python_version} ${taiga::back::install_dir}",
+    command => "${taiga::back::virtualenv} --system-site-packages -p ${taiga::back::python_path}${taiga::back::python_version} ${taiga::back::install_dir}",
     creates => "${taiga::back::install_dir}/lib/python${taiga::back::python_version}/site-packages/django",
     user    => $taiga::back::user,
   }
