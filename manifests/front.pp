81da38362632f3d398f05d6c9f1ec91ee7ee2815
Romain Tartière <romain@blogreen.org>
2019-02-21 11:15:54 -1000

    Add data-types for all parameters

--- manifests/front.pp
+++ manifests/front.pp
@@ -1,15 +1,15 @@
 class taiga::front (
-  $back_hostname,
-  $back_protocol,
-  $events = false,
-  $user = 'nobody',
-  $repo_ensure = 'present',
-  $repo_revision = 'stable',
-  $install_dir = '/srv/www/taiga-front',
-  $default_language = 'en',
-  $public_register_enabled = true,
-  $ldap_enable = false,
-  $gravatar = true,
+  String[1]                 $back_hostname,
+  Enum['http', 'https']     $back_protocol,
+  Boolean                   $events = false,
+  String[1]                 $user = 'nobody',
+  Enum['present', 'latest'] $repo_ensure = 'present',
+  String[1]                 $repo_revision = 'stable',
+  Stdlib::Absolutepath      $install_dir = '/srv/www/taiga-front',
+  String[2, 2]              $default_language = 'en',
+  Boolean                   $public_register_enabled = true,
+  Boolean                   $ldap_enable = false,
+  Boolean                   $gravatar = true,
 ) {
   include ::taiga::front::repo
   include ::taiga::front::config
