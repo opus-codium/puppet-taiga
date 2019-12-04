36a0623474badc7acc050f7a1cbec0cb4bdc74ff
Romain Tartière <romain@blogreen.org>
2019-12-04 10:58:09 -1000

    Fix checking if virtualenv is installed
    While here, retrigger install and migration if virtualenv needed to do
    something (e.g. the system is upgraded and we use a new Python version).

--- manifests/back/env.pp
+++ manifests/back/env.pp
@@ -3,7 +3,11 @@ class taiga::back::env {
 
   exec { 'taiga-back-virtualenv':
     command => "${taiga::back::virtualenv} -p ${taiga::back::python_path}${taiga::back::python_version} ${taiga::back::install_dir}",
-    creates => "${taiga::back::install_dir}/lib/python${taiga::back::python_version}/site-packages/django",
+    creates => "${taiga::back::install_dir}/bin/python${taiga::back::python_version}",
     user    => $taiga::back::user,
+    notify  => [
+      Class['taiga::back::install'],
+      Class['taiga::back::migrate'],
+    ],
   }
 }
