61052e918b12f502b9c2bb9a374175019e9e7744
Romain Tartière <romain@blogreen.org>
2021-02-01 12:23:56 -1000

    Add a basic unit test

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'puppetlabs_spec_helper/module_spec_helper'
+
+require 'rspec-puppet-facts'
+include RspecPuppetFacts # rubocop:disable Style/MixinUsage
+
+RSpec.configure do |c|
+  c.hiera_config = 'spec/fixtures/hiera/hiera.yaml'
+end
